f1c8bc37e28dd38a62f3a85b373037a75e9de400
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2025-05-14 13:18:18 +0000

    deleting components and functions I dont need

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,29 +1,10 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
 			token: sessionStorage.getItem("token") || null,
 			privateData: null
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-
 
 			// Sign up a new user
 			signup: async (username, email, password, name, last_name, date_of_birth) => {
